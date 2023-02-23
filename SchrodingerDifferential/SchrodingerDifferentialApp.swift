3f63388042801e78e20fb984fe1953a79a692a67
Tim Stack <tstack43215@yahoo.com>
2023-02-23 12:53:43 -0600

    Initial Commit

---
+++ SchrodingerDifferential/SchrodingerDifferentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrodingerDifferentialApp.swift
+//  SchrodingerDifferential
+//
+//  Created by IIT PHYS 440 on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SchrodingerDifferentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
